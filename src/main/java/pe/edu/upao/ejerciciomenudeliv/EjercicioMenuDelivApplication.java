057d17260941d1170895503bea5ed3bf917911df
ajeampiergg <ajeampiergg2@gmail.com>
2023-04-23 22:33:36 -0500

    • "feat: Definir endpoints para permitir a los usuarios ver el menú de comida y hacer un pedido."

---
+++ src/main/java/pe/edu/upao/ejerciciomenudeliv/EjercicioMenuDelivApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.ejerciciomenudeliv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioMenuDelivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioMenuDelivApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upao/ejerciciomenudeliv/EjercicioMenuDelivApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.ejerciciomenudeliv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioMenuDelivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
